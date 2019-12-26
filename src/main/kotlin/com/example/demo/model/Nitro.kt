2070bed8084e9c3f88bc5193af7649d036e32650
ZhenyaSUPREME <evgeniy.goncharov.98@inbox.ru>
2019-12-26 05:03:19 +0300

    create speed type Int

--- src/main/kotlin/com/example/demo/model/Nitro.kt
+++ src/main/kotlin/com/example/demo/model/Nitro.kt
@@ -3,4 +3,4 @@ package com.example.demo.model
 import org.springframework.data.mongodb.core.mapping.Document
 
 @Document
-data class Nitro(var id:String,var name:String)
+data class Nitro(var id:String,var name:String,var speed:Int)
