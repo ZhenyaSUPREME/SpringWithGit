9313ae25dff1b834a53875722345407b01701053
ZhenyaSUPREME <evgeniy.goncharov.98@inbox.ru>
2019-12-26 05:12:14 +0300

    create list type string

--- src/main/kotlin/com/example/demo/repository/NitroRepository.kt
+++ src/main/kotlin/com/example/demo/repository/NitroRepository.kt
@@ -3,4 +3,6 @@ package com.example.demo.repository
 import com.example.demo.model.Nitro
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository
 
-interface NitroRepository:ReactiveMongoRepository<Nitro,String>
+interface NitroRepository:ReactiveMongoRepository<Nitro,String>{
+    fun list(id:String)
+}
